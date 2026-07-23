722a8b4de7b726353a19ae367f8d832cd77dedef
mvolv27-alt <mvolv27@gmail.com>
2026-07-23 19:45:04 -0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "direct-promocoes-v1";
+const CACHE_NAME = "direct-promocoes-v2";
 const CORE_ASSETS = ["/", "/manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
